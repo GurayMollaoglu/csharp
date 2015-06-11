b2d575a9660bb2e4b1e1751bb7180bbe33272ad7
Erdal ÖZBAN <eozban@hotmail.com>
2015-06-11 14:51:37 +0300

    v1

---
+++ IBANKontrol/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IBANKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyIBAN myIBAN = new MyIBAN(textBox1.Text);
+
+            if (myIBAN.IBAN_Status)
+                MessageBox.Show("IBAN DOĞRU");
+            else
+                MessageBox.Show("IBAN YANLIŞ");
+        }
+    }
+}
